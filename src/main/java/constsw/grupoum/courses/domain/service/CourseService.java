f40a29f22a3552c64fbc00fa8c7d57cebf91ef02
Guilherme Luz <guilhermeluzds@gmail.com>
2023-05-14 16:29:06 -0300

    feat: adicionado Topic entity

--- src/main/java/constsw/grupoum/courses/domain/service/CourseService.java
+++ src/main/java/constsw/grupoum/courses/domain/service/CourseService.java
@@ -1,7 +1,9 @@
 package constsw.grupoum.courses.domain.service;
 
+import java.util.List;
 import java.util.UUID;
 
+import constsw.grupoum.courses.domain.entity.Course;
 import org.springframework.stereotype.Service;
 
 import constsw.grupoum.courses.domain.dto.CourseDTO;
@@ -22,6 +24,18 @@ public class CourseService {
 
         try {
             return courseMapper.courseToCourseDTO(courseRepository.findById(id).orElse(null));
+        } catch (Exception e) {
+            throw new CourseException(e);
+        }
+
+    }
+
+    public List<CourseDTO> getAll() throws CourseException {
+
+        try {
+            List<Course> courses = courseRepository.findAll();
+            List<CourseDTO> coursesDTO = (List<CourseDTO>) courseMapper.collectionUsertoCollectionResponseUsers(courses);
+            return coursesDTO;
         } catch (Exception e) {
             throw new CourseException(e);
         }
