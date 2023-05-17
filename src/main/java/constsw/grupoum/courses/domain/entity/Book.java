3786b568cb21b9197fa42e0739342b02aa994e70
jhonatapers <jhonatapers@gmail.com>
2023-05-17 19:27:58 -0300

    Anotation parar Criar Document books

--- src/main/java/constsw/grupoum/courses/domain/entity/Book.java
+++ src/main/java/constsw/grupoum/courses/domain/entity/Book.java
@@ -1,11 +1,13 @@
 package constsw.grupoum.courses.domain.entity;
 
+import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import lombok.Data;
 
 @Data
+@Document(collection = "books", language = "json")
 public class Book {
 
     @MongoId
