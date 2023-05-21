dd0845481b8a9bb215435fc5afc92927651926d1
jhonatapers <jhonatapers@gmail.com>
2023-05-21 14:09:49 -0300

    exception + validacao

--- src/main/java/constsw/grupoum/courses/domain/service/CourseService.java
+++ src/main/java/constsw/grupoum/courses/domain/service/CourseService.java
@@ -5,10 +5,13 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import constsw.grupoum.courses.domain.dto.BookRefDTO;
 import constsw.grupoum.courses.domain.dto.CourseDTO;
 import constsw.grupoum.courses.domain.entity.Course;
 import constsw.grupoum.courses.domain.exception.CourseException;
+import constsw.grupoum.courses.domain.exception.InvalidBookException;
 import constsw.grupoum.courses.domain.mapper.CourseMapper;
+import constsw.grupoum.courses.domain.repository.BookRepository;
 import constsw.grupoum.courses.domain.repository.CourseRepository;
 import constsw.grupoum.courses.domain.vo.QueryParam;
 import lombok.RequiredArgsConstructor;
@@ -16,6 +19,8 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 @Service
 public class CourseService {
+
+    private final BookRepository bookRepository;
 
     private final CourseRepository courseRepository;
 
@@ -34,6 +39,9 @@ public class CourseService {
     }
 
     public CourseDTO updateCourse(UUID id, CourseDTO courseDTO) throws CourseException {
+
+        validateBooks(courseDTO.bibliography());
+
         Course course = courseMapper.courseDTOToCourse(courseDTO);
         course.setId(id);
 
@@ -41,6 +49,9 @@ public class CourseService {
     }
 
     public CourseDTO createCourse(CourseDTO course) throws CourseException {
+
+        validateBooks(course.bibliography());
+
         return courseMapper
                 .courseToCourseDTO(courseRepository.insert(courseMapper.courseDTOWithoutIdToCourseWithId(course)));
     }
@@ -53,6 +64,23 @@ public class CourseService {
         } catch (Throwable e) {
             throw new CourseException(e);
         }
+    }
+
+    private void validateBooks(Collection<BookRefDTO> books) throws InvalidBookException {
+
+        Boolean erro = false;
+        StringBuilder sb = new StringBuilder();
+
+        for (BookRefDTO book : books) {
+            if (bookRepository.findById(book.isbn13()).isEmpty()) {
+                erro = true;
+                sb.append(",");
+                sb.append(book.isbn13());
+            }
+        }
+
+        if (erro)
+            throw new InvalidBookException(String.format("IBNS's %s not found", sb.toString()));
     }
 
 }
